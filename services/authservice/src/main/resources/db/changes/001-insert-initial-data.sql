799ff76dcf551d2348db7547bf973d985afde411
fedorov <fedorov@nemosoft.ru>
2025-11-21 17:01:55 +0300

    Доработана авторизация

--- services/authservice/src/main/resources/db/changes/001-insert-initial-data.sql
+++ services/authservice/src/main/resources/db/changes/001-insert-initial-data.sql
@@ -1,3 +1,14 @@
-insert into roles (name) values ('ROLE_USER');
-insert into users (email, name, password) values ('email', '1234', '$2a$10$4u7X1sJ8A6YGnFhDag5/GuHVvH4jYmuHKTeTV6VXC1m1E5FC98BOm');
-insert into users_roles (user_id, role_id) values (1,1);
+INSERT INTO roles (name)
+VALUES ('ROLE_USER')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO users (email, name, password)
+VALUES ('email', '1234', '$2a$10$4u7X1sJ8A6YGnFhDag5/GuHVvH4jYmuHKTeTV6VXC1m1E5FC98BOm')
+ON CONFLICT (email) DO NOTHING;
+INSERT INTO users_roles (user_id, role_id)
+
+SELECT u.id, r.id
+FROM users u
+JOIN roles r ON r.name = 'ROLE_USER'
+WHERE u.email = 'email'
+ON CONFLICT DO NOTHING;
